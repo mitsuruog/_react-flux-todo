ef6537d566997e5b7c7f270fb1e1aeea716b5eed
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2015-12-30 23:11:05 +0900

    Initial commit

---
+++ src/components/App/index.js
@@ -0,0 +1,13 @@
+import React, { Component, PropTypes } from 'react'
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>Hello React</div>
+    )
+  }
+
+};
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  context: __dirname + "/src",
+  entry: {
+    jsx: "./index.jsx",
+    html: "./index.html",
+  },
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js",
+  },
+  module: {
+    loaders: [
+      { test: /\.html$/, loader: "file?name=[name].[ext]" },
+//      { test: /\.css$/, loader: "style-loader!css-loader?modules&importLoaders=1!postcss-loader" },
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, loaders: ["react-hot","babel-loader"]},
+    ],
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
+}
